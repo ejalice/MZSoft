dc9df6cfa74ea86183c6f2867283350bcac811f7
김상현 <kshy1019@naver.com>
2022-10-07 20:36:25 +0900

    XCode Project init

---
+++ MZSoft/MZSoft/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MZSoft
+//
+//  Created by 김상현 on 2022/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
