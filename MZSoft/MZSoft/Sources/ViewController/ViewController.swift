e0a835500718b4b708422a777a319f2ecf289128
김상현 <kshy1019@naver.com>
2022-10-08 00:27:25 +0900

    init view

--- MZSoft/MZSoft/Sources/ViewController/ViewController.swift
+++ MZSoft/MZSoft/Sources/ViewController/ViewController.swift
@@ -4,16 +4,30 @@
 //
 //  Created by 김상현 on 2022/10/07.
 //
-
+import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
 
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "Hello World!"
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(label)
+        configureContraints()
     }
-
+    
+    private func configureContraints() {
+        label.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
 
 }
 
