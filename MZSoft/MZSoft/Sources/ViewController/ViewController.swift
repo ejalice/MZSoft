9bedf6c5f6f72d1b9644838c8f6a759a2ab9747f
MBSoo <qud9783@khu.ac.kr>
2022-10-09 16:47:58 +0900

    [Feat] #2 RootView를 HomeView로 수정했습니다.

--- MZSoft/MZSoft/Sources/ViewController/ViewController.swift
+++ MZSoft/MZSoft/Sources/ViewController/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let vc = PrologViewController()
+        let vc = HomeViewController()
         vc.modalPresentationStyle = .fullScreen
         self.present(vc, animated: false)
     }
